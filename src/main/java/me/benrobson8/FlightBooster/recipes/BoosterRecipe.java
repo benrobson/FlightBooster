c5a84f4cd0063264e91f616307c87ddea1b12156
benrobson8 <csceg12188>
2019-07-01 13:55:34 +1000

    Finished recipe with the information added.

--- src/main/java/me/benrobson8/FlightBooster/recipes/BoosterRecipe.java
+++ src/main/java/me/benrobson8/FlightBooster/recipes/BoosterRecipe.java
@@ -1,12 +1,17 @@
 package me.benrobson8.FlightBooster.recipes;
 
 import me.benrobson8.FlightBooster.FlightBoosterMain;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
 
 public class BoosterRecipe implements Listener {
     FlightBoosterMain plugin;
@@ -15,18 +20,23 @@ public class BoosterRecipe implements Listener {
     }
 
     public void BoosterRecipe() {
-        ItemStack item = new ItemStack(Material.SLIME_BALL, 1);
+        ItemStack item = new ItemStack(Material.FEATHER, 1);
+
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.AQUA + "Flight Booster");
+        meta.addEnchant(Enchantment.DURABILITY, 3, true);
+
+        ArrayList<String> lore = new ArrayList<String>();
+        lore.add("Right click to be elevated for flight.");
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+
         NamespacedKey key = new NamespacedKey(plugin, "Booster");
         ShapelessRecipe br = new ShapelessRecipe(key, item);
 
         br.addIngredient(Material.FEATHER);
         br.addIngredient(Material.FIREWORK_ROCKET);
 
-
-//        br.shape("$$$","@%@","@%@");
-//        br.setIngredient('$', Material.DIAMOND_BLOCK);
-//        br.setIngredient('@', Material.AIR);
-//        br.setIngredient('%', Material.CONDUIT);
         plugin.getServer().addRecipe(br);
     }
 }
